7643633ab0250bf42e0e03e2b3335c84971f6962
Mario <6395323+mario-huang@users.noreply.github.com>
2025-01-27 22:40:26 +0800

    use ./venv/bin/python -m pip

--- install-dependencies-macos.sh
+++ install-dependencies-macos.sh
@@ -7,4 +7,4 @@ rm ./venv/miniconda.sh
 
 sed -i '' 's|^\(--extra-index-url.*\)|# \1|' requirements.txt
 
-./venv/bin/pip install -r requirements.txt
+./venv/bin/python -m pip install -r requirements.txt
