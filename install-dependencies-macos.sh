af853eb1121b720d129d71010f6206c031275d17
Mario <6395323+mario-huang@users.noreply.github.com>
2025-01-27 17:01:52 +0800

    sed before source

--- install-dependencies-macos.sh
+++ install-dependencies-macos.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-rm -rf ./venv && mkdir ./venv
+rm -rf ./venv
+mkdir ./venv
 curl https://repo.anaconda.com/miniconda/Miniconda3-py310_24.7.1-0-MacOSX-arm64.sh -o ./venv/miniconda.sh
 bash ./venv/miniconda.sh -b -u -p ./venv
 rm ./venv/miniconda.sh
 
-source ./venv/bin/activate
-
 sed -i '' 's|^\(--extra-index-url.*\)|# \1|' requirements.txt
+
+source ./venv/bin/activate
 pip install -r requirements.txt
